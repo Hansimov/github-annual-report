b23d2aa7ddfe054406ad2a2301c82ee9e9c8e854
Hansimov <591172499@qq.com>
2023-12-18 16:15:30 +0800

    :gem: [Feature] Auto get_node_id by username

--- secrets_reader.py
+++ secrets_reader.py
@@ -1,5 +1,7 @@
-from getpass import getpass
 import json
+import requests
+
+from getpass import getpass
 from pathlib import Path
 
 
@@ -13,17 +15,41 @@ class SecretsReader:
             self.secrets = {}
         self.load()
 
+    def get_node_id(self):
+        api_url = f"https://api.github.com/users/{self.username}"
+        headers = {
+            "Authorization": f"Bearer {self.token}",
+            "X-GitHub-Api-Version": "2022-11-28",
+        }
+        try:
+            response = requests.get(api_url, headers=headers)
+            data = response.json()
+            return data["node_id"]
+        except:
+            return None
+
     def load(self):
-        for key in ["username", "token"]:
+        for key in ["username", "token", "node_id"]:
             if key in self.secrets and self.secrets.get(key):
                 setattr(self, key, self.secrets[key])
             else:
-                if key == "token":
-                    val = getpass(f"Enter your Github {key}: ")
-                else:
+                if key == "username":
                     val = input(f"Enter your Github {key}: ")
+                elif key == "token":
+                    val = getpass(f"Enter your Github {key}: ")
+                elif key == "node_id":
+                    val = self.get_node_id()
+                else:
+                    raise KeyError(f"{key}")
 
                 setattr(self, key, val)
                 self.secrets[key] = val
                 with open(self.secrets_path, "w") as wf:
                     json.dump(self.secrets, wf, ensure_ascii=False, indent=4)
+
+
+if __name__ == "__main__":
+    secrets_reader = SecretsReader()
+    print(f"username: {secrets_reader.username}")
+    print(f"token:    {secrets_reader.token}")
+    print(f"node_id:  {secrets_reader.node_id}")
